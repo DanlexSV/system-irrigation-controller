eb76b429e98bab50936f3f91c6f9bdb66f619bcb
DanlexSV <danilibra15@hotmail.com>
2025-05-09 19:04:54 +0200

    Main WIFI Detector

---
+++ main/main.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+//const int sensorPin = 34;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+
+  Serial.println("==== Información del dispositivo ====");
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+  //Serial.println("Iniciando lectura de humedad...");
+}
+
+void loop() {
+  /*
+  int sensorValue = analogRead(sensorPin);
+  
+  int humedad = map(sensorValue, 0, 4095, 100, 0);
+  
+  Serial.print("Valor bruto: ");
+  Serial.print(sensorValue);
+  Serial.print(" | Humedad: ");
+  Serial.print(humedad);
+  Serial.println("%");
+
+  delay(2000);
+  */
+}
