8fd7ec31d3941306becd7d47f208ec9de840a830
DanlexSV <danilibra15@hotmail.com>
2025-05-18 18:12:30 +0200

    Sin comentarios en codigo

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,38 @@
-#define SENSOR_PIN 35        // A0 del sensor FC-28 al pin GPIO 35
-#define RELAY_PIN 26         // IN1 del relé al pin GPIO 26
-#define HUMEDAD_UMBRAL 3000  // Ajusta según tu sensor y pruebas
-#define DURACION_RIEGO 9000 // 9 segundos en milisegundos
+#define SENSOR_PIN 35
+#define RELAY_PIN 26
+#define HUMEDAD_UMBRAL 3000
+#define DURACION_RIEGO 9000
 
 bool bombaEncendida = false;
 unsigned long tiempoInicioRiego = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(SENSOR_PIN, INPUT);     // Pin del sensor como entrada
-  pinMode(RELAY_PIN, OUTPUT);     // Pin del relé como salida
-  digitalWrite(RELAY_PIN, HIGH);  // Bomba apagada (relé inactivo)
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
   Serial.println("Sistema de riego automático listo.");
 }
 
 void loop() {
-  int humedad = analogRead(SENSOR_PIN);  // Leer valor analógico del sensor
+  int humedad = analogRead(SENSOR_PIN);
   Serial.print("Humedad del suelo (ADC): ");
   Serial.println(humedad);
 
   unsigned long ahora = millis();
 
-  // Si el suelo está seco y la bomba está apagada
   if (!bombaEncendida && humedad > HUMEDAD_UMBRAL) {
     Serial.println("Humedad baja. Activando bomba...");
-    digitalWrite(RELAY_PIN, LOW);  // Relé activo (bomba encendida)
+    digitalWrite(RELAY_PIN, LOW);
     tiempoInicioRiego = ahora;
     bombaEncendida = true;
   }
 
-  // Si la bomba ya está encendida y pasó el tiempo de riego
   if (bombaEncendida && ahora - tiempoInicioRiego >= DURACION_RIEGO) {
     Serial.println("Tiempo cumplido. Apagando bomba.");
-    digitalWrite(RELAY_PIN, HIGH); // Relé inactivo (bomba apagada)
+    digitalWrite(RELAY_PIN, HIGH);
     bombaEncendida = false;
   }
 
-  delay(1000);  // Espera 1 segundo antes de la próxima lectura
+   delay(1000);
 }
